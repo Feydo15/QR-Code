2640698e3643c3413b26ad184a2f7a429dae4f04
Feydo15 <feydollor@gmail.com>
2023-02-16 12:56:36 +0200

    first

---
+++ qr-code/src/App.js
@@ -0,0 +1,23 @@
+import QR from './images/image-qr-code.png';
+// import bg from "./images/favicon-32x32.png"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" >
+      <div className="container">
+      <div className="App-card">
+        <img src={QR} className="App-img" alt="QR" />
+        <h2>
+          Edit <code>src/App.js</code> and save to reload.
+        </h2>
+        <p>
+        Edit and save to reload. usdnfklsnnsdng.nesgfnwsebgnnsg.
+        </p>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
